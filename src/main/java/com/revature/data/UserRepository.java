6f76cce99d24aa033b2815f0342cbde755b1ea02
Mark Kolb <99706832+TheToasterRoaster@users.noreply.github.com>
2022-03-23 16:25:54 -0400

    interface

--- src/main/java/com/revature/data/UserRepository.java
+++ src/main/java/com/revature/data/UserRepository.java
@@ -1,5 +1,9 @@
 package com.revature.data;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.revature.models.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+	
 }
